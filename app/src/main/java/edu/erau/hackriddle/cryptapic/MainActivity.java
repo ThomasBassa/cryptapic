43ef66a50627e9c29d5788597ec34243b3c0b70c
NewittAll <newittr@gmail.com>
2016-10-15 23:04:05 -0400

    Linked "I already have an account" button to new login page

--- app/src/main/java/edu/erau/hackriddle/cryptapic/MainActivity.java
+++ app/src/main/java/edu/erau/hackriddle/cryptapic/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.erau.hackriddle.cryptapic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void attemptLogin(View view) {
+
+    }
+
+    public void recoverAccount(View view) {
+        Intent accountExists = new Intent(this, AccountExsists.class);
+        startActivity(accountExists);
     }
 }
