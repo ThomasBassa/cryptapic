44d48286947fba35826a51ffbbd64c7a1adff7a3
Thomas Bassa <ThomasBassa@users.noreply.github.com>
2016-10-16 01:37:08 -0400

    Write a proper keysExist method in MainActivity

--- app/src/main/java/edu/erau/hackriddle/cryptapic/MainActivity.java
+++ app/src/main/java/edu/erau/hackriddle/cryptapic/MainActivity.java
@@ -3,9 +3,16 @@ package edu.erau.hackriddle.cryptapic;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,12 +31,13 @@ public class MainActivity extends AppCompatActivity {
 
         if (keysExist()) {
             userField.setVisibility(View.GONE);
-            accountButton.setVisibility(View.GONE);
+            //accountButton.setVisibility(View.GONE);
         }
     }
 
     private boolean keysExist() {
-        return true; //true; TODO real condition-- do keys exist
+        File home = getFilesDir();
+        return Arrays.asList(home.listFiles()).contains(new File(home, "userKeys"));
     }
 
     public void attemptLogin(View view) {
@@ -39,7 +47,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void recoverAccount(View view) {
-        Intent accountExists = new Intent(this, AccountExists.class);
-        startActivity(accountExists);
+        //This is all test code for the keysExist method
+        File testFile = new File(getFilesDir(), "userKeys");
+        if (keysExist()) {
+            if (!testFile.delete()) {
+                Log.wtf("cryptapic", "Couldn't delete test file though exists?");
+            }
+            Toast.makeText(this, "Keys exist-- deleted", Toast.LENGTH_SHORT).show();
+        } else {
+            try (FileOutputStream stream = new FileOutputStream(testFile)) {
+                stream.write(0x30);
+            } catch (IOException e) {
+                Toast.makeText(this, "Key creation failed", Toast.LENGTH_SHORT)
+                        .show();
+                Log.e("cryptapic", e.toString(), e);
+            }
+            Toast.makeText(this, "Keys created", Toast.LENGTH_SHORT).show();
+        }
+
+        //"Actual" code
+        //Intent accountExists = new Intent(this, AccountExists.class);
+        //startActivity(accountExists);
     }
 }
