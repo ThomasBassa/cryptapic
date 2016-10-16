919c93e8009875c55e6d5b95b62e9940b430a68c
Thomas Bassa <ThomasBassa@users.noreply.github.com>
2016-10-16 01:03:29 -0400

    Hide unnecessary elements in main when account "exists"
    Exists check is a stub for now

--- app/src/main/java/edu/erau/hackriddle/cryptapic/MainActivity.java
+++ app/src/main/java/edu/erau/hackriddle/cryptapic/MainActivity.java
@@ -1,20 +1,35 @@
 package edu.erau.hackriddle.cryptapic;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText userField;
     private EditText passField;
+    private Button accountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userField = (EditText) findViewById(R.id.editUsername);
+        passField = (EditText) findViewById(R.id.editPassword);
+        accountButton = (Button) findViewById(R.id.accountButton);
+
+        if (keysExist()) {
+            userField.setVisibility(View.GONE);
+            accountButton.setVisibility(View.GONE);
+        }
+    }
+
+    private boolean keysExist() {
+        return true; //true; TODO real condition-- do keys exist
     }
 
     public void attemptLogin(View view) {
