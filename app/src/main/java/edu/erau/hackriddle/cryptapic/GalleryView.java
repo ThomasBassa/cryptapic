0aeda8ba22c5bf8b216e9330cc83cbd4207d47bb
NewittAll <newittr@gmail.com>
2016-10-15 23:41:32 -0400

    Created GalleryView

--- app/src/main/java/edu/erau/hackriddle/cryptapic/GalleryView.java
+++ app/src/main/java/edu/erau/hackriddle/cryptapic/GalleryView.java
@@ -3,29 +3,29 @@ package edu.erau.hackriddle.cryptapic;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.Toast;
 
 public class GalleryView extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gallery_view);
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.main);
+            setContentView(R.layout.activity_gallery_view);
 
             GridView gridview = (GridView) findViewById(R.id.gridview);
             gridview.setAdapter(new ImageAdapter(this));
 
-            gridview.setOnItemClickListener(new OnItemClickListener() {
+            gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 public void onItemClick(AdapterView<?> parent, View v,
                                         int position, long id) {
-                    Toast.makeText(HelloGridView.this, "" + position,
+                    Toast.makeText(GalleryView.this, "" + position,
                             Toast.LENGTH_SHORT).show();
                 }
             });
         }
     }
-}
