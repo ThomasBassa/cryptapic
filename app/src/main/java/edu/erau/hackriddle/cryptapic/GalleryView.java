9d201ef6937f34eb48ac037962df70b139135e00
NewittAll <newittr@gmail.com>
2016-10-15 23:25:14 -0400

    Added "Username" text field to "accountExists" page

--- app/src/main/java/edu/erau/hackriddle/cryptapic/GalleryView.java
+++ app/src/main/java/edu/erau/hackriddle/cryptapic/GalleryView.java
@@ -1,7 +1,10 @@
 package edu.erau.hackriddle.cryptapic;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class GalleryView extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class GalleryView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery_view);
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main);
+
+            GridView gridview = (GridView) findViewById(R.id.gridview);
+            gridview.setAdapter(new ImageAdapter(this));
+
+            gridview.setOnItemClickListener(new OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View v,
+                                        int position, long id) {
+                    Toast.makeText(HelloGridView.this, "" + position,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
